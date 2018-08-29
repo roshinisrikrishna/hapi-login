83812d58dd9eb60a5af22e81bee9ad5528f30814
roshinisrikrishna <38094811+roshinisrikrishna@users.noreply.github.com>
2018-08-29 14:54:27 +0530

    Update user.js

--- schemas/user.js
+++ schemas/user.js
@@ -9,18 +9,15 @@ module.exports = {
         email: Joi.string().optional()
     }),
     returnUser: Joi.object({//schema for output user details
-        // Item: Joi.object({
-        userPassword: Joi.string().required(),
-        userDOB: Joi.string().required(),
-        userEmail: Joi.string().required(),
-        userId: Joi.string().required(),
+        createdAt: Joi.number().required(),
+        password: Joi.string().required(),
+        id: Joi.string().required(),
+        name: Joi.string().required(),
         updatedAt: Joi.number().required(),
-        userName: Joi.string().required(),
-        createdAt: Joi.number().required()
-        //})
+        dob: Joi.string().optional(),
+        mail: Joi.string().optional()
     }),
     givenid: Joi.object({//schema for input user id
-        
         id: Joi.string().required()
     }),
     responseSchema: Joi.object({//schema for response message
